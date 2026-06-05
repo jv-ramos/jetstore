7340773b0eb6a96603ec0e7bd539db884a5acf97
Jetulio <joao.ramos0793@gmail.com>
2026-06-05 17:56:46 -0300

    chore: edited model and migration

--- app/Models/Promotion.php
+++ app/Models/Promotion.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+#[Fillable('product_id', 'discount_percentage', 'start_date', 'end_date')]
 class Promotion extends Model
 {
     /** @use HasFactory<\Database\Factories\PromotionFactory> */
--- database/migrations/2026_06_04_163519_create_promotions_table.php
+++ database/migrations/2026_06_04_163519_create_promotions_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('promotions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->decimal('discount_percentage', 5, 2);
+            $table->date('start_date');
+            $table->date('end_date');
             $table->timestamps();
         });
     }
