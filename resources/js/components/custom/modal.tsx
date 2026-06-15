6836d7230215619651376479b62e39c7b6dc4556
Jetulio <joao.ramos0793@gmail.com>
2026-06-15 10:47:18 -0300

    chore: update on dashboard modal "Do not show this again" button and logic

--- resources/js/components/custom/modal.tsx
+++ resources/js/components/custom/modal.tsx
@@ -7,24 +7,36 @@ import {
 
 interface ModalProps {
     children: {
+        note: string;
         title: string;
         body: string;
         comment: string;
     };
     open: boolean;
     onClose: () => void;
+    turnOff: () => void;
 }
 
-export default function Modal({ children, open, onClose }: ModalProps) {
+export default function Modal({ children, open, onClose, turnOff }: ModalProps) {
     return (
         <Dialog open={open} onOpenChange={onClose}>
             <DialogContent>
                 <DialogHeader>
-                    <DialogTitle className="font-2xl text-[#ae6ff7]">{children.title}</DialogTitle>
+                    <DialogTitle className="font-2xl text-[#ae6ff7]">
+                        {children.title}
+                    </DialogTitle>
                 </DialogHeader>
-                <p className="font-sl text-neutral-400"><span className="text-red-400">NOTE: </span>{children.note}</p>
+                {children.note ? (
+                    <p className="font-sl text-neutral-400">
+                        <span className="text-red-400">NOTE: </span>
+                        {children.note}
+                    </p>
+                ) : null}
                 <p>{children.body}</p>
-                <span className="font-sl text-neutral-600">{children.comment}</span>
+                <span className="font-sl text-neutral-600">
+                    {children.comment}
+                </span>
+                <button onClick={turnOff}>Do not show this again</button>
             </DialogContent>
         </Dialog>
     );
