d0444aaa436eb9f814f5dad9da8dc5b617f669b7
Jetulio <joao.ramos0793@gmail.com>
2026-06-29 10:23:09 -0300

    chore: update to order details page

--- resources/js/pages/orders/show.tsx
+++ resources/js/pages/orders/show.tsx
@@ -1,23 +1,47 @@
 import { usePage } from '@inertiajs/react';
+import { useEffect, useState } from 'react';
+import OrderDetailsContainer from '@/components/custom/order-details-container';
+import { fetchProductById } from '@/services/api';
 
 export default function OrderDetails() {
     const { order } = usePage().props as any;
 
-    console.log(order);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [productFound, setProductFound] = useState(null);
+    const [fakeApiProductFound, setfakeApiProductFound] = useState(null);
+    // const [productFound, setProductFound] = useState(null);
+    // const [fakeApiProductFound, setfakeApiProductFound] = useState(null);
+    // const [counter, setCounter] = useState(1);
+
+    useEffect(() => {
+        async function loadProduct(productId: string): Promise<void> {
+            setLoading(true);
+            setError(null);
+
+            try {
+                const data = order.order_items;
+                const fakeApiProduct = await fetchProductById(
+                    data[0].product_id,
+                );
+
+                setProductFound(data[0].product);
+                setfakeApiProductFound(fakeApiProduct);
+            } catch (error) {
+                console.error('Error loading products:', error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        loadProduct(order.order_items[0].product_id);
+    }, [order.order_items[0].product_id]);
 
     return (
-        <div>
-            <h1>{order.order_number}</h1>
-            <p>Status: {order.status}</p>
-            {order.order_items?.map((item: any, i: number) => (
-                <div key={i} className="flex gap-4">
-                    <img src={item.product?.image} alt={item.product?.title} className="h-18 w-18 object-contain" />
-                    <div>
-                        <p>{item.product?.title ?? item.name}</p>
-                        <p>Qtd: {item.quantity} × R$ {item.amount}</p>
-                    </div>
-                </div>
-            ))}
-        </div>
+        <OrderDetailsContainer
+            productFound={productFound}
+            fakeApiProductFound={fakeApiProductFound}
+            error={error}
+            loading={loading}
+        />
     );
 }
